a962e8661a855a371a7ce96575c1e87ba07b9689
IdanLnrd <idan@lnrd.io>
2021-07-29 21:41:56 +0300

    ref

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import { Messages } from "./utils/messages";
 import { Logger } from "./utils/logger";
-
-const port: number = Number(process.env.PORT) || 3000;
+import { config } from 'dotenv';
+config();
+const { PORT } = process.env;
+const port = Number(PORT);
 
 const app = express();
 app.use(cors());
